aa8d10329fe9c96e81204afd3786acbb494b4a90
Maheshwary Narkhede <147603457+MaheshwaryNarkhede@users.noreply.github.com>
2025-03-12 22:59:55 +0530

    Update page.jsx

--- app/(main)/journal/write/page.jsx
+++ app/(main)/journal/write/page.jsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
-import dynamic from "next/dynamic";
+export const dynamic = 'force-dynamic'; 
 import { useForm, Controller } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
