f80c153167bb38b58336597569c0ce1bdcf6d679
Maheshwary Narkhede <147603457+MaheshwaryNarkhede@users.noreply.github.com>
2025-03-12 22:31:03 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import Header from "../components/header";
 import "./globals.css";
-import {Inter} from "next/font/google";
+// import {Inter} from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
 const inter=Inter({subsets:["Latin"]});
