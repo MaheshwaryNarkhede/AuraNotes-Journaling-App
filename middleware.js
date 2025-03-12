55548006b4a5fca56bf4d0840e2b34fcc81b2b30
Maheshwary Narkhede <147603457+MaheshwaryNarkhede@users.noreply.github.com>
2025-03-12 22:50:00 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -44,11 +44,15 @@ const clerk = clerkMiddleware(async (auth, req) => {
 export default createMiddleware(aj, clerk);
 
 // Keep your existing matcher config for consistency
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+//     // Always run for API routes
+//     "/(api|trpc)(.*)",
+//   ],
+// };
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ['/api/:path*'],
+  runtime: 'nodejs', // instead of edge
 };
