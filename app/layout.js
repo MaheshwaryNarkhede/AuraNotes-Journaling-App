597d569b1ba262dd71096ca124a8a4053dea2dde
Maheshwary Narkhede <147603457+MaheshwaryNarkhede@users.noreply.github.com>
2025-03-12 22:38:18 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import {Inter} from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const inter=Inter({subsets:["Latin"]});
+const inter=Inter({subsets:["latin"]});
 
 export const metadata = {
   title: "AuraNotes",
